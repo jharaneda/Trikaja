6c775f3bdecae2a4b2f7a04fe994d7449eed87d8
Jhonatan Araneda <jharanedac@gmail.com>
2021-10-22 15:56:42 -0700

    Added TicketHardwareModel class with its attributes and getters/setters

---
+++ src/main/java/com/csis3275/model/TicketHardwareModel_jar_86.java
@@ -0,0 +1,77 @@
+package com.csis3275.model;
+
+public class TicketHardwareModel_jar_86 {
+	
+	private int id;
+	private String creationDate;
+	private String status;
+	private String userCreator;
+	private String assigneeUser;
+	private String type;
+	private String priority;
+	private String position;
+	private String hardwareToBeChanged;
+	private String comments;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCreationDate() {
+		return creationDate;
+	}
+	public void setCreationDate(String creationDate) {
+		this.creationDate = creationDate;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getUserCreator() {
+		return userCreator;
+	}
+	public void setUserCreator(String userCreator) {
+		this.userCreator = userCreator;
+	}
+	public String getAssigneeUser() {
+		return assigneeUser;
+	}
+	public void setAssigneeUser(String assigneeUser) {
+		this.assigneeUser = assigneeUser;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getPriority() {
+		return priority;
+	}
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	public String getHardwareToBeChanged() {
+		return hardwareToBeChanged;
+	}
+	public void setHardwareToBeChanged(String hardwareToBeChanged) {
+		this.hardwareToBeChanged = hardwareToBeChanged;
+	}
+	public String getComments() {
+		return comments;
+	}
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+	
+}
