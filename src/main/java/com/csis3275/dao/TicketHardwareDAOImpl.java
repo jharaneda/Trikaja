c00d2cfc6894bf72f58e58345eb601b9ff51f12b
Jhonatan Araneda <jharanedac@gmail.com>
2021-10-22 16:44:35 -0700

    Added TicketHardwareDAO with getAllTicket method

---
+++ src/main/java/com/csis3275/dao/TicketHardwareDAOImpl.java
@@ -0,0 +1,30 @@
+package com.csis3275.dao;
+
+import java.util.ArrayList;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import com.csis3275.model.TicketHardwareModel_jar_86;
+
+@Service
+public class TicketHardwareDAOImpl {
+	JdbcTemplate jdbcTemplate;
+	
+	private final String GET_TICKET_HARDWARE = "SELECT * FROM ticketHardware";
+	
+	public TicketHardwareDAOImpl(DataSource dataSource) {
+		jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
+	public ArrayList<TicketHardwareModel_jar_86> getAllTickets(){
+		ArrayList<TicketHardwareModel_jar_86> allTickets = new ArrayList<TicketHardwareModel_jar_86>();
+		
+		allTickets = jdbcTemplate.query(GET_TICKET_HARDWARE, new TicketHardwareRowMapper_jar_86());
+		
+		return allTickets;
+	}
+
+}
