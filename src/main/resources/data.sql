3f847b19b3b13bebfd918fb9374de50de0a8937d
Jhonatan Araneda <jharanedac@gmail.com>
2021-10-26 23:20:24 -0700

    SQL Statements updated. There werent correlated to each other and there was garbage data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,18 +1,60 @@
-insert into tickets (creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged, commentsID) values ('2020-12-12 03:38:28', 'pending', 'jhattigan0', 'csuerz0', 'software', 'normal', 1, 'Colville','1');
-insert into tickets (creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged, commentsID) values ('2021-03-22 05:48:51', 'solved', 'dbeveridge1', 'rricioppo1', 'software', 'normal', 35, 'Black or African American', '1');
-insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged, commentsID) values ('2021-08-06 08:34:13', 'solved', 'lcadge2', 'rjoselin2', 'software', 'high', 24, 'Shoshone', '1');
-insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged, commentsID) values ('2021-03-09 15:13:13', 'pending', 'gtidd3', 'tlynn3', 'software', 'normal', 47, 'Chinese', '1');
-insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged, commentsID) values ('2020-11-14 02:34:25', 'open', 'jkilbourne4', 'mmughal4', 'hardware', 'normal', 18, 'Paiute', '1');
-insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged, commentsID) values ('2021-10-21 14:18:45', 'solved', 'fjuan5', 'kgentsch5', 'software', 'normal', 18, 'Bangladeshi', '1');
-insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged, commentsID) values ('2021-07-27 02:03:06', 'open', 'shatchette6', 'pgabala6', 'software', 'high', 31, 'Creek', '1');
-insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged, commentsID) values ('2021-01-09 23:34:28', 'solved', 'kbream7', 'lorange7', 'hardware', 'high', 32, 'Uruguayan', '1');
-insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged, commentsID) values ('2020-10-24 06:30:25', 'open', 'rjeans8', 'tboase8', 'software', 'normal', 40, 'Puget Sound Salish', '1');
-insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged, commentsID) values ('2021-03-27 06:46:56', 'open', 'edallimare9', 'cmonini9', 'software', 'low', 35, 'Ottawa', '1');
-insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged, commentsID) values ('2020-12-20 01:38:13', 'pending', 'aconninghama', 'ssothama', 'hardware', 'low', 31, 'Spaniard', '1');
-insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged, commentsID) values ('2021-07-29 12:30:20', 'pending', 'dboowb', 'talvesb', 'hardware', 'normal', 37, 'Asian', '1');
-insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged, commentsID) values ('2021-10-06 03:31:09', 'open', 'dhanselmanc', 'ashevillc', 'hardware', 'low', 18, 'Alaskan Athabascan', '1');
-insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged, commentsID) values ('2021-08-02 06:54:48', 'open', 'nwhaymand', 'heverald', 'software', 'normal', 30, 'Uruguayan', '1');
-insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged, commentsID) values ('2021-06-18 16:46:16', 'solved', 'ngoodbodye', 'jrayworthe', 'hardware', 'low', 23, 'Pima', '1');
+insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged) values ('2021-09-13 21:50:08', 'Open', 'jhunnam0', 'dgounel0', 'Hardware', 'Low', 12, 'HeadSet');
+insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged) values ('2021-09-20 08:55:12', 'Pending', 'cwingate1', 'mkaradzas1', 'Hardware', 'Low', 39, 'HeadSet');
+insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged) values ('2021-03-13 02:58:26', 'Solved', 'mpostan2', 'lmacarthur2', 'Software', 'High', 34, 'Screen');
+insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged) values ('2021-04-08 10:17:57', 'Solved', 'ccasement3', 'udytham3', 'Software', 'High', 41, 'Screen');
+insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged) values ('2021-03-20 05:40:37', 'Open', 'ppucker4', 'vostridge4', 'Software', 'Normal', 50, 'Screen');
+insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged) values ('2021-02-22 03:46:10', 'Pending', 'maltamirano5', 'bjerram5', 'Software', 'Normal', 35, 'Webcam');
+insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged) values ('2021-04-11 01:36:38', 'Solved', 'btonsley6', 'splatts6', 'Hardware', 'Low', 32, 'Keyboard');
+insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged) values ('2020-10-26 18:06:50', 'Solved', 'acutress7', 'kwapples7', 'Hardware', 'High', 43, 'Webcam');
+insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged) values ('2020-11-03 17:11:01', 'Open', 'hlampitt8', 'hprall8', 'Software', 'Normal', 15, 'Webcam');
+insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged) values ('2021-05-04 06:46:23', 'Pending', 'rpalatino9', 'rmcilvoray9', 'Software', 'Normal', 44, 'N/A');
+insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged) values ('2020-11-08 01:45:03', 'Open', 'yroxburcha', 'fschouthedea', 'Hardware', 'High', 6, 'N/A');
+insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged) values ('2021-07-24 02:01:14', 'Pending', 'mannettb', 'cpickettb', 'Hardware', 'High', 39, 'HeadSet');
+insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged) values ('2021-02-26 23:35:18', 'Pending', 'ltawnc', 'bhurdwellc', 'Software', 'Normal', 35, 'Mouse');
+insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged) values ('2021-07-30 13:52:00', 'Solved', 'iclamptond', 'cjoselevitchd', 'Hardware', 'Normal', 40, 'Keyboard');
+insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged) values ('2021-08-19 05:55:11', 'Open', 'nmacgilpatricke', 'rkearneye', 'Hardware', 'Normal', 48, 'Tower');
+insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged) values ('2021-08-31 07:10:17', 'Solved', 'dcranagef', 'mspeariettf', 'Hardware', 'Low', 26, 'Keyboard');
+insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged) values ('2020-11-08 17:49:19', 'Solved', 'ahuxtableg', 'sberettag', 'Hardware', 'High', 35, 'Tower');
+insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged) values ('2021-03-03 11:05:59', 'Pending', 'bmaturah', 'ecornfooth', 'Hardware', 'Normal', 33, 'Mouse');
+insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged) values ('2020-11-24 00:35:36', 'Open', 'sducki', 'mhaysomi', 'Hardware', 'High', 4, 'Tower');
+insert into tickets(creationDate, status, userCreator, assigneeUser, typeOfTicket, priority, position, hardwareToBeChanged) values ('2020-12-28 19:29:56', 'Solved', 'tmacpeicej', 'ccostleyj', 'Hardware', 'Low', 48, 'HeadSet');
 
-insert into comments (ticketID, creationDate, creator, commentType, comment) values (1, '2021-10-22 22:52:03', 'jharanedac', 'public', 'some large text 1 some large text 1 some large text 1 some large text 1 some large text 1 some large text 1 some large text 1 some large text 1 some large text 1 some large text 1 some large text 1 some large text 1 some large text 1 some large text 1 some large text 1 some large text 1 ');
-insert into comments (ticketID, creationDate, creator, commentType, comment) values (1, '2021-10-22 23:52:03', 'jharanedac', 'public', 'some large text 2 ');
+
+insert into comments (id, ticketID, creationDate, creator, commentType, comment) values (1, 1, '2020-12-03 23:54:34', 'cpacher0', 'public', 'In congue. Etiam justo. Etiam pretium iaculis justo.');
+insert into comments (id, ticketID, creationDate, creator, commentType, comment) values (2, 2, '2021-03-17 22:39:51', 'dbursnell1', 'public', 'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.');
+insert into comments (id, ticketID, creationDate, creator, commentType, comment) values (3, 3, '2021-01-11 23:20:51', 'tbruckent2', 'public', 'Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.
+
+Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.');
+insert into comments (id, ticketID, creationDate, creator, commentType, comment) values (4, 4, '2021-07-21 16:44:47', 'fcurton3', 'public', 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.
+
+Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.');
+insert into comments (id, ticketID, creationDate, creator, commentType, comment) values (5, 5, '2021-10-20 16:14:37', 'rburras4', 'public', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.');
+insert into comments (id, ticketID, creationDate, creator, commentType, comment) values (6, 6, '2021-07-25 20:07:08', 'eravenscroftt5', 'public', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.
+
+Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.');
+insert into comments (id, ticketID, creationDate, creator, commentType, comment) values (7, 7, '2021-08-12 00:37:40', 'cobbard6', 'public', 'Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.');
+insert into comments (id, ticketID, creationDate, creator, commentType, comment) values (8, 8, '2021-10-25 20:42:25', 'bbenbough7', 'public', 'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.');
+insert into comments (id, ticketID, creationDate, creator, commentType, comment) values (9, 9, '2021-02-20 18:09:39', 'mmishow8', 'public', 'Sed ante. Vivamus tortor. Duis mattis egestas metus.
+
+Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.');
+insert into comments (id, ticketID, creationDate, creator, commentType, comment) values (10, 10, '2021-06-02 18:36:17', 'acreavin9', 'public', 'Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.');
+insert into comments (id, ticketID, creationDate, creator, commentType, comment) values (11, 11, '2020-11-16 20:27:48', 'rloudyana', 'public', 'Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.
+
+In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.');
+insert into comments (id, ticketID, creationDate, creator, commentType, comment) values (12, 12, '2021-07-25 23:50:19', 'kpinsentb', 'public', 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.');
+insert into comments (id, ticketID, creationDate, creator, commentType, comment) values (13, 13, '2021-04-14 21:00:06', 'zairec', 'public', 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.
+
+Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.');
+insert into comments (id, ticketID, creationDate, creator, commentType, comment) values (14, 14, '2021-01-16 15:44:08', 'qcasbond', 'public', 'Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.
+
+In congue. Etiam justo. Etiam pretium iaculis justo.');
+insert into comments (id, ticketID, creationDate, creator, commentType, comment) values (15, 15, '2021-03-10 12:57:53', 'rshimonye', 'public', 'Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.');
+insert into comments (id, ticketID, creationDate, creator, commentType, comment) values (16, 16, '2021-01-25 15:40:35', 'gcoenf', 'public', 'Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.
+
+Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.');
+insert into comments (id, ticketID, creationDate, creator, commentType, comment) values (17, 17, '2020-12-02 15:29:14', 'rcamellig', 'public', 'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.');
+insert into comments (id, ticketID, creationDate, creator, commentType, comment) values (18, 18, '2021-02-15 12:40:18', 'kmalcolmsonh', 'public', 'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.');
+insert into comments (id, ticketID, creationDate, creator, commentType, comment) values (19, 19, '2021-05-12 11:13:28', 'omcdavidi', 'public', 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.
+
+Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.');
+insert into comments (id, ticketID, creationDate, creator, commentType, comment) values (20, 20, '2021-01-31 17:40:31', 'cbannonj', 'public', 'Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.');
