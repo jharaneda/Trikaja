37bb2cc3d07aff3bf7ce2cc220530a2e1c17387b
KadynNeale <nealek1@student.douglascollege.ca>
2021-11-07 18:44:12 -0800

    created inventory view shell; created empty inventory view controller

---
+++ src/main/java/com/csis3275/controller/InventoryController_kne_58.java
@@ -0,0 +1,15 @@
+package com.csis3275.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class InventoryController_kne_58 {
+
+	@RequestMapping("/inventory")
+	public String showInventory_kne_58() {
+		
+		
+		return "inventoryView-kne-58";
+	}
+}
